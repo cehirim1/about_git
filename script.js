55b5aeead428219f88dbcff1dec0d4fbd49ffc77
chisom ehirim <humbled1st@gmail.com>
2023-12-21 18:21:39 -0800

    i committing new and modified files

---
+++ script.js
@@ -0,0 +1,14 @@
+/*basic git commands 
+
+git is a fancy term for tracking changes
+enables you to take snapshots and save points by making a commit - commit creates a save point
+
+
+- we need a special folder to put snapshots - create using git init (hidden repository, .git)
+- to see all folders including hidden - ls -la
+- git add - gathers all folders that we need to a staging area. we can individual file or *.html (all html extensions) . adds everything including the hidden folder
+- git commit - saves a snapshot of everything that is in staging into the hidden file
+- 
+
+
+*/
