1a74349e08ef56a875c991ba58402d50772ee3cd
chisom ehirim <humbled1st@gmail.com>
2023-12-21 23:11:09 -0800

    committed changes

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ enables you to take snapshots and save points by making a commit - commit create
 - to see all folders including hidden - ls -la
 - git add - gathers all folders that we need to a staging area. we can individual file or *.html (all html extensions) . adds everything including the hidden folder
 - git commit - saves a snapshot of everything that is in staging into the hidden file
-- 
-
+- git :wq used to save and exit text editor if you accidentally commit without message
+- branch code to experiment so as not to bork all your code
+- git status to see the branch you're on 
+- git branch <branch name> to create new branch
+- git checkout <branch name> to go to new branch
 
 */
